788b9d8abe8c66d595c5d72e0fcf1a320c173115
ty33-CR7 <yuto.tsujimoto33@gmail.com>
2025-06-19 19:14:36 +0900

    epsilon  5~50 ADR _result

--- experiments/2025-06-19_exp01/code/run_all.py
+++ experiments/2025-06-19_exp01/code/run_all.py
@@ -33,39 +33,40 @@ preprocess_adult_data(labeled=False,bins=L)  # rawから処理
 #繰り返し回数
 N=10
 
-for i in range(N):
-    #ユーザ側の処理
-    df=pd.read_csv("../data/processed/discrete_30_adult.csv")
-    X=df.drop(["income"],axis=1)
-    y=df.loc[:,"income"]  
-    with open('../data/processed/domain_dict.json', 'r', encoding='utf-8') as f:
-        domain_dict = json.load(f)
-            # 保存ディレクトリ準備
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    out_dir = os.path.abspath(os.path.join(BASE_DIR, "..", "data", "external", "dist", f"epsilon{epsilon:.2f}",f"{i}"))
-    os.makedirs(out_dir, exist_ok=True)
-    user_process_DR(X,domain_dict,y,epsilon,d,out_dir)
+for epsilon in [5,7,10,12,15,17,20,30,40,50]: 
+    for i in range(N):
+        #ユーザ側の処理
+        df=pd.read_csv("../data/processed/discrete_30_adult.csv")
+        X=df.drop(["income"],axis=1)
+        y=df.loc[:,"income"]  
+        with open('../data/processed/domain_dict.json', 'r', encoding='utf-8') as f:
+            domain_dict = json.load(f)
+                # 保存ディレクトリ準備
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        out_dir = os.path.abspath(os.path.join(BASE_DIR, "..", "data", "external", "dist", f"epsilon{epsilon:.2f}",f"{i}"))
+        os.makedirs(out_dir, exist_ok=True)
+        user_process_DR(X,domain_dict,y,epsilon,d,out_dir)
 
 
-    #収集者側の処理
-    file_path=f"../data/external/domain/epsilon{epsilon:.2f}/{i}/ADR_domain_T_{low_threshold}_L_{max_interval_len}.csv"
-    # 各属性に対して処理を実行
-    dict_domain={}
-    for column in adult_column:
-        if column in NUM_COLUMNS:
-            column_type = "numerical"
-        elif column in CAT_COLUMNS:
-            column_type = "categorical"
-        else:
-            raise ValueError(f"column は 'numerical' または 'categorical' の型で指定してください: {column}")
+        #収集者側の処理
+        file_path=f"../data/external/domain/epsilon{epsilon:.2f}/{i}/ADR_domain_T_{low_threshold}_L_{max_interval_len}.csv"
+        # 各属性に対して処理を実行
+        dict_domain={}
+        for column in adult_column:
+            if column in NUM_COLUMNS:
+                column_type = "numerical"
+            elif column in CAT_COLUMNS:
+                column_type = "categorical"
+            else:
+                raise ValueError(f"column は 'numerical' または 'categorical' の型で指定してください: {column}")
 
-        cross_table = pd.read_csv(f"../data/external/dist/epsilon{epsilon:.2f}/{i}/{column}_OUE_estimation.csv",index_col=0)
-        result = Attribute_Domain_Reconstruction(
-            cross_table,
-            low_threshold,
-            max_interval_len,
-            column_type
-        )
-        dict_domain[column]=result
-    os.makedirs(f"../data/external/domain/epsilon{epsilon:.2f}/{i}", exist_ok=True)
-    dict_domain_to_json(dict_domain,file_path)
+            cross_table = pd.read_csv(f"../data/external/dist/epsilon{epsilon:.2f}/{i}/{column}_OUE_estimation.csv",index_col=0)
+            result = Attribute_Domain_Reconstruction(
+                cross_table,
+                low_threshold,
+                max_interval_len,
+                column_type
+            )
+            dict_domain[column]=result
+        os.makedirs(f"../data/external/domain/epsilon{epsilon:.2f}/{i}", exist_ok=True)
+        dict_domain_to_json(dict_domain,file_path)
